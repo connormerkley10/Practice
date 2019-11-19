3d61065a546b648e3d3153607cab47bd8f298043
connormerkley10 <48034371+connormerkley10@users.noreply.github.com>
2019-11-19 16:24:15 -0700

    This is my first change

--- Midterm/Midterm/Controllers/HomeController.cs
+++ Midterm/Midterm/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 // Author: Connor Merkley
 // Desc: This website is intended to store a users digital media collection. The website is built off .NET framework using C#.
 
+//This also a test using github
+
 namespace Midterm.Controllers
 {
     public class HomeController : Controller
